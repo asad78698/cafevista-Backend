e620f6a80ab2bf00efe6a42fdb835408219e1b52
Muhammad Asad <108513936+asad78698@users.noreply.github.com>
2024-07-14 11:47:24 +0500

    Add files via upload

---
+++ DatabaseModels/newsLetterModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Newsletter = new mongoose.Schema({
+    Email: {
+        type: String,
+        unique: true
+    }
+}) // Creating a schema for the newsletter model.
+
+
+const newsLetterModel = mongoose.model('newsLetter', Newsletter)
+
+
+module.exports = newsLetterModel    // Exporting the model so that it can be used in other files.
+
